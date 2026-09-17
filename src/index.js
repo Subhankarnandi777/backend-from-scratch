82ccff017f4f0ca4dca580c73d76a869be79c4b4
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-09-17 18:50:23 +0530

    mongodb connected

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import dotenv from "dotenv";
+import dns from "node:dns";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './.env'
+})
+
+dns.setServers([process.env.MONGODB_DNS_SERVER || "1.1.1.1"])
+
+if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is not configured in .env")
+}
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express";
+const app = express()
+
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI} / ${DB_NAME}`)
+        app.on("error" , (error) => {
+            console.log("ERROR" , error);
+            throw error   
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+            
+        })
+
+    } catch (error) {
+        console.error("Error" , error);
+        throw err
+    }
+})()
+
+*/
